a060b3064f37562081c82659d32139f69e429e0e
Markos Rodrigo <markos.rodrigo@outlook.com>
2020-01-19 19:00:31 -0300

    feat: initial commit

---
+++ auth-resource/src/main/java/org/helppet/auth/resource/configuration/ResourceSecurityConfiguration.java
@@ -0,0 +1,7 @@
+package org.helppet.auth.resource.configuration;
+
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class ResourceSecurityConfiguration {
+}
